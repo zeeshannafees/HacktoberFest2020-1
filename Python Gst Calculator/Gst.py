6bc0e580672267b63d9b5515e61ef36b113b92b0
Rishikesh <contactrishikeshhere@gmail.com>
2020-09-29 13:03:14 +0530

    added the gst project

---
+++ Python Gst Calculator/Gst.py
@@ -0,0 +1,9 @@
+# modify this or make a better one
+Original_price = #get input from user
+Net_price = #get input  from user
+GST_amount = Net_price - Original_price
+
+GST_percent = ((GST_amount * 100) / Original_price)
+print("GST = ",end='') 
+#add line to print GST_percent
+print("%")
