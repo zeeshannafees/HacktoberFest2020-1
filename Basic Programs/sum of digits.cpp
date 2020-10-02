f5a7cc1699859255a25e5a789189d29381f93897
Rishikesh kumar <57762992+Rishikesh147@users.noreply.github.com>
2020-10-02 13:19:01 +0530

    Update sum of digits.cpp
    increased the readability of the code

--- Basic Programs/sum of digits.cpp
+++ Basic Programs/sum of digits.cpp
@@ -4,15 +4,20 @@
 using namespace std;  
 int main()  
 {  
-int n,sum=0,m;    
-cout<<"Enter a number: ";    
-cin>>n;    
-while(n>0)    
-{    
-m=n%10;    
-sum=sum+m;    
-n=n/10;    
-}    
-cout<<"Sum is= "<<sum<<endl;    
+    int number,sum=0,remainder;    
+    cout<<"Enter a number: ";    
+    cin>>number;    
+    while(number>0)    
+    {   
+        //getting digits from right to left
+        remainder = number % 10; 
+
+        // adding the above remainder 
+        sum = sum + remainder; 
+
+        //here we're reducing the number    
+        number = number / 10;    
+    }    
+    cout<<"Sum is= "<<sum<<endl;    
 return 0;  
 }  
